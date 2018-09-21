5e172b933343d4a16bd16b4fc8aa0d321e35cf95
Connor Barley <connorwbarley@gmail.com>
2018-09-21 16:44:28 +0000

    set up environment variables

--- app.js
+++ app.js
@@ -16,8 +16,9 @@ var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
 
-mongoose.connect(process.env.DATABASEURL);
-// mongoose.connect("mongodb://cbarley:BaileyBarley123!@ds111063.mlab.com:11063/yelpcamp");
+var url = process.env.DATABASEURL || "mongodb://localhost/yelp_camp"
+mongoose.connect(url);
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
