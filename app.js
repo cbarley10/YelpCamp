fda785f8950801080106724a796419937294e744
Connor Barley <connorwbarley@gmail.com>
2018-09-21 16:36:05 +0000

    update dbURL

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ var express        = require("express"),
 var commentRoutes    = require("./routes/comments"),
     campgroundRoutes = require("./routes/campgrounds"),
     indexRoutes      = require("./routes/index")
-    
-// mongoose.connect("mongodb://localhost/yelp_camp");
-mongoose.connect("mongodb://cbarley:BaileyBarley123!@ds111063.mlab.com:11063/yelpcamp");
+
+mongoose.connect(process.env.DATABASEURL);
+// mongoose.connect("mongodb://cbarley:BaileyBarley123!@ds111063.mlab.com:11063/yelpcamp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname + "/public"));
